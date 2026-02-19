270b929f1c94f49ab2931de089faab4c6aa76de9
Mohammad osama <152876079+Mohammadosama55@users.noreply.github.com>
2026-02-19 23:33:52 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (85.64%) - LeetSync

---
+++ 65-valid-number/valid-number.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean isNumber(String s) {
+        s = s.trim();
+        
+        if (s.length() == 0) return false;
+        
+        boolean hasE = false;
+        boolean hasDecimal = false;
+        boolean hasDigitBeforeE = false;
+        boolean hasDigitAfterE = false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c >= '0' && c <= '9') {
+                if (hasE) {
+                    hasDigitAfterE = true;
+                } else {
+                    hasDigitBeforeE = true;
+                }
+            }
+            else if (c == '.') {
+                if (hasDecimal || hasE) {
+                    return false;
+                }
+                hasDecimal = true;
+            }
+            else if (c == 'e' || c == 'E') {
+                if (hasE || !hasDigitBeforeE) {
+                    return false;
+                }
+                hasE = true;
+                hasDigitAfterE = false;
+            }
+            else if (c == '+' || c == '-') {
+               
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        
+        
+        return hasDigitBeforeE && (!hasE || hasDigitAfterE);
+    }
+}
