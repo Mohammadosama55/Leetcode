a43e09c448bb43d330083733521ed12c9b33a559
Mohammad osama <152876079+Mohammadosama55@users.noreply.github.com>
2026-02-25 23:32:57 +0530

    Time: 2 ms (88.27%) | Memory: 44 MB (35.80%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        
+ 
+        int[] count = new int[26];
+        boolean[] visited = new boolean[26];
+        Stack<Character> stack = new Stack<>();
+
+       
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : s.toCharArray()) {
+           
+            count[c - 'a']--;
+
+           
+            if (visited[c - 'a']) continue;
+
+          
+            while (!stack.isEmpty() && c < stack.peek() && count[stack.peek() - 'a'] > 0) {
+                visited[stack.pop() - 'a'] = false;
+            }
+
+            stack.push(c);
+            visited[c - 'a'] = true;
+        }
+
+        
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
