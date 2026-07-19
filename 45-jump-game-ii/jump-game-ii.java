2b5df25c0c502acb271d8b8449257f7b47c7b369
Mohammad osama <152876079+Mohammadosama55@users.noreply.github.com>
2026-07-19 22:27:17 +0530

    Time: 1 ms (99.72%) | Memory: 47.4 MB (38.82%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+        if(n<=1){
+            return 0;
+        }
+        int f=0;
+        int c=0;
+        int j=0;
+        for(int i=0;i<n-1;i++){
+            f=Math.max(f,i+nums[i]);
+            if(i==c){
+                j++;
+                c=f;
+            }
+
+        }
+        return j;
+        
+    }
+}
