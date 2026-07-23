65e2dc3f0d548d489992db8e2f6d60f1a3494519
Mohammad osama <152876079+Mohammadosama55@users.noreply.github.com>
2026-07-23 14:35:25 +0530

    Time: 1 ms (80.17%) | Memory: 43.5 MB (13.55%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,21 +1,28 @@
 class Solution {
     public int numDecodings(String s) {
-        if(s==null || s.length()==0 || s.charAt(0)=='0') return 0;
-       
+        if(s==null ||s.length()==0 || s.charAt(0)=='0'){
+            return 0;
+        }
         int n=s.length();
-        int []dp=new int[n+1];
+        int dp[]=new int[n+1];
         dp[0]=1;
         dp[1]=1;
         for(int i=2;i<=n;i++){
-            int oneDigit=s.charAt(i-1)-'0';
-            if(oneDigit >=1){
-                dp[i] +=dp[i-1];
+            int sc=s.charAt(i-1);
+            if(sc !='0'){
+                dp[i]+=dp[i-1];
             }
-            int twoDigit=Integer.parseInt(s.substring(i-2,i));
-            if(twoDigit>=10 && twoDigit<=26){
+            
+            int ssc=Integer.parseInt(s.substring(i-2,i));
+            if(ssc >=10 && ssc <=26 ){
                 dp[i]+=dp[i-2];
             }
+            
+
+
         }
         return dp[n];
+
+        
     }
 }
