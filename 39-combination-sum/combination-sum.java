18082ac40e4cbc0b2a459d1b8c7f761828b86eae
Mohammad osama <152876079+Mohammadosama55@users.noreply.github.com>
2026-07-26 20:35:23 +0530

    Time: 2 ms (96.47%) | Memory: 45.7 MB (51.66%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result=new ArrayList<>();
+        backtracking(candidates,target,new ArrayList<>(),0,result);
+        return result;
+        
+    }
+    private void  backtracking(int []nums,int t,List<Integer>cur,int start,List<List<Integer>> result){
+        if(t==0){
+            result.add(new ArrayList<>(cur));
+            return;
+        }
+        if(t<0){
+            return ;
+        }
+        for(int i=start;i<nums.length;i++){
+            if (nums[i] > t) {
+                continue; 
+            }
+            cur.add(nums[i]);
+             backtracking(nums,t-nums[i],cur,i,result);
+            cur.remove(cur.size()-1);
+        }
+    }
+}
