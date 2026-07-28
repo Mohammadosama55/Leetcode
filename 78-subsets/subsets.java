4e77617940ade8fe4a7a72aa921649bccb2837f6
Mohammad osama <152876079+Mohammadosama55@users.noreply.github.com>
2026-07-28 19:15:31 +0530

    Time: 1 ms (86.79%) | Memory: 44.4 MB (6.97%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,15 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>result=new ArrayList<>();
-        backTracking(nums,0,new ArrayList<>(),result);
+        backtracking(nums,0,new ArrayList<>(),result);
+        
         return result;
         
     }
-    private void backTracking(int[]nums,int start,List<Integer>cur,List<List<Integer>>result){
-        result.add(new ArrayList(cur));
+    private void  backtracking(int[] nums,int start,List<Integer>cur, List<List<Integer>>result){
+        result.add(new ArrayList<>(cur));
         for(int i=start;i<nums.length;i++){
+             if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
             cur.add(nums[i]);
-            backTracking(nums,i+1,cur,result);
+             backtracking(nums,i+1,cur,result);
             cur.remove(cur.size()-1);
         }
     }
