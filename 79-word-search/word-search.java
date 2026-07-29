447561ab8a623cbda55895c83da0cfc450ec0db5
Mohammad osama <152876079+Mohammadosama55@users.noreply.github.com>
2026-07-30 00:02:28 +0530

    Time: 139 ms (49.04%) | Memory: 43.1 MB (28.80%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,38 +1,31 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        if(board==null ||board.length==0||board[0].length==0){
+            return false;
+        }
         int m=board.length;
         int n=board[0].length;
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]==word.charAt(0)){
-                    if(backtrack(board,word,i,j,0)){
-                        return true;
-                    }
+                if(dfs(board,word,i,j,0)){
+                    return true;
                 }
             }
         }
         return false;
         
     }
-    private boolean backtrack(char[][] board,String word,int r,int c,int index){
-        if(index== word.length()){
+    private boolean dfs(char[][]board,String word,int i,int j,int index){
+        if(index==word.length()){
             return true;
         }
-        if(r<0 || r >= board.length || c<0 || c>=board[0].length || board[r][c]!=word.charAt(index)){
+        if(i<0 ||i>=board.length ||j<0||j>=board[0].length|| board[i][j]!=word.charAt(index)){
             return false;
         }
-
-        char temp = board[r][c];
-        board[r][c]='#';
-
-        
-       boolean found = backtrack(board, word, r + 1, c, index + 1) 
-                    || backtrack(board, word, r - 1, c, index + 1) 
-                    || backtrack(board, word, r, c + 1, index + 1) || 
-                       backtrack(board, word, r, c - 1, index + 1);
-
-        board[r][c]=temp;
-        return found;   
+       char temp=board[i][j];
+        board[i][j]='#';
+        boolean found=dfs(board,word,i+1,j,index +1) || dfs(board,word,i-1,j,index +1) || dfs(board,word,i,j+1,index +1) || dfs(board,word,i,j-1,index +1);
+        board[i][j]=temp;
+        return found;
     }
 }
