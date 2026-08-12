ea5dfcbe3beabc069ba032bcf9e380728f66ed9b
Mohammad osama <152876079+Mohammadosama55@users.noreply.github.com>
2026-08-12 21:09:17 +0530

    Time: 90 ms (89.48%) | Memory: 129.2 MB (13.74%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int groupSize) {
+       if(hand.length % groupSize !=0){
+            return false;
+        }
+        Arrays.sort(hand);
+        HashMap<Integer,Integer>f=new HashMap<>();
+        for(int card:hand){
+            f.put(card,f.getOrDefault(card,0)+1);
+        }
+        for(int card:hand){
+            if(f.get(card)==0){
+                continue;
+            }
+            for(int x=card;x<card+groupSize;x++){
+                if(f.getOrDefault(x,0)==0){
+                    return false;
+                }
+                f.put(x,f.get(x)-1);
+            }
+        }
+        return true;
+        
+    }
+}
