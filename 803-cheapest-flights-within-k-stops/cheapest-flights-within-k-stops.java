9977dd1ea5f06d1201ccb007415d7d70d67df166
Mohammad osama <152876079+Mohammadosama55@users.noreply.github.com>
2026-09-04 15:27:01 +0530

    Time: 7 ms (43.47%) | Memory: 46.6 MB (54.85%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+       
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        
+        for (int i = 0; i <= k; i++) {
+           
+            int[] temp = Arrays.copyOf(dist, n);
+
+            for (int[] flight : flights) {
+                int u = flight[0];
+                int v = flight[1];
+                int price = flight[2];
+
+                if (dist[u] != Integer.MAX_VALUE) {
+                    if (dist[u] + price < temp[v]) {
+                        temp[v] = dist[u] + price;
+                    }
+                }
+            }
+
+            dist = temp;
+        }
+
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+}
